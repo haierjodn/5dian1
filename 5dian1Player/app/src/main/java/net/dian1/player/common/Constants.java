7b35c155346bd232b81b75fb668e325286de7650
Desmond <haierjodn@gmail.com>
2015-11-12 10:15:30 +0800

    add center high light

--- 5dian1Player/app/src/main/java/net/dian1/player/common/Constants.java
+++ 5dian1Player/app/src/main/java/net/dian1/player/common/Constants.java
@@ -7,6 +7,6 @@ public class Constants {
 
     public final static String ACTION_MAX_PLAY_TIMES_LIMITED = "net.5dian1.player.times.limited";
 
-    public final static String URL_VIP_INRO = "http://www.5dian1.net";
+    public final static String URL_VIP_INRO = "http://www.5dian1.net/app/jinzhuan.html";
 
 }
